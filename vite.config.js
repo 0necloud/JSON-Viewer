7e3941a2b9aefd8b6ee235ad1ce9ce7f6439c026
0necloud <hewyiyun@gmail.com>
2026-03-27 15:11:21 +0800

    Configure GitHub Pages deployment via Actions and update Vite base path

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/JSON-Viewer/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
